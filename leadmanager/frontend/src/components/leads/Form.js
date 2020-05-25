4970a3b7904ff50a07d293bcda52a7f898e71412
Tim Jones <timjonez@protonmail.com>
2020-05-25 14:12:55 -0400

    added error handling

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLead } from '../../actions/leads';
@@ -8,62 +9,70 @@ export class Form extends Component {
         name: '',
         email: '',
         message: '',
+    };
 
-    }
     static propTypes = {
-        addLead: PropTypes.func.isRequired
-    }
+        addLead: PropTypes.func.isRequired,
+    };
 
-    onChange = e => this.setState({ [e.target.name]: e.target.value });
-    onSubmit = e => {
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit = (e) => {
         e.preventDefault();
         const { name, email, message } = this.state;
         const lead = { name, email, message };
         this.props.addLead(lead);
-    }
+        this.setState({
+            name: '',
+            email: '',
+            message: '',
+        });
+    };
 
     render() {
         const { name, email, message } = this.state;
         return (
-            <div className='card card-body mt-4 mb-4'>
-                <h1>Add Lead</h1>
+            <div className="card card-body mt-4 mb-4">
+                <h2>Add Lead</h2>
                 <form onSubmit={this.onSubmit}>
-                    <div className='form-group'>
+                    <div className="form-group">
                         <label>Name</label>
                         <input
-                            className='form-control'
-                            type='text'
-                            name='name'
+                            className="form-control"
+                            type="text"
+                            name="name"
                             onChange={this.onChange}
                             value={name}
                         />
                     </div>
-                    <div className='form-group'>
+                    <div className="form-group">
                         <label>Email</label>
                         <input
-                            className='form-control'
-                            type='email'
-                            name='email'
+                            className="form-control"
+                            type="email"
+                            name="email"
                             onChange={this.onChange}
                             value={email}
                         />
                     </div>
-                    <div className='form-group'>
+                    <div className="form-group">
                         <label>Message</label>
-                        <input
-                            className='form-control'
-                            type='text'
-                            name='message'
+                        <textarea
+                            className="form-control"
+                            type="text"
+                            name="message"
                             onChange={this.onChange}
                             value={message}
                         />
                     </div>
-                    <div className='form-group'>
-                        <button type='submit' className='btn btn-primary'>Submit</button>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+            </button>
                     </div>
                 </form>
             </div>
-        )
+        );
     }
 }
 
